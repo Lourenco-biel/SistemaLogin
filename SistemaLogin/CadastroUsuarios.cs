55136affc31a1eaa9bdb84fc9df5581ebb475982
gabriel ibiapino <Lourencogabriel77@gmail.com>
2021-12-06 23:39:46 -0300

    Adicionar arquivos de projeto.

---
+++ SistemaLogin/CadastroUsuarios.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaLogin
+{
+    class CadastroUsuarios
+    {
+        private static Usuario[] usuarios =
+        {
+
+            new Usuario (){Nome = "Gabriel",Senha = "abc123"},
+            new Usuario (){Nome = "Dnny", Senha = "123abc"},
+            new Usuario (){Nome = " Bola", Senha = "abcd"}
+
+        };
+
+        private static Usuario _userLogado = null;
+
+        public static Usuario UsuarioLogado
+        {
+            get { return _userLogado;  }
+            private set { _userLogado = value; }
+        }
+
+        public static bool  Login (string nome, string senha)
+        {
+            foreach (Usuario usuario in usuarios)
+            {
+                if (usuario.Nome == nome && usuario.Senha == senha)
+                {
+                    UsuarioLogado = usuario;
+                    return true;
+                }
+            }
+
+            return false; 
+        }
+
+
+    }
+}
